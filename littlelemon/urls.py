84dc8d722eb4bc4e1e73a3a2d0c6f009e4d6abf1
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-07-30 17:06:13 +0800

    Update urls.py

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework import routers
 from restaurant import views
 from rest_framework.authtoken.views import obtain_auth_token
-import .settings
+from .settings import DEBUG
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 router = routers.DefaultRouter()
