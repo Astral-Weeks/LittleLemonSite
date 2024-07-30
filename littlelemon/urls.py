ba74e8c10f62d594f9bb4f9c2eded86a936cab24
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-07-30 13:15:57 +0800

    Update urls.py

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import routers
 from restaurant import views
 from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 router = routers.DefaultRouter()
 # router.register(r'users', views.UsersView)
@@ -17,3 +18,5 @@ urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
     path('api-token-auth/', obtain_auth_token),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
