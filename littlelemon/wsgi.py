9a5fb79308b479ef49552db2586c4f3046599bfc
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-07-31 09:16:46 +0800

    Update wsgi.py

--- littlelemon/wsgi.py
+++ littlelemon/wsgi.py
@@ -14,8 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'littlelemon.settings')
 
 application = get_wsgi_application()
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
